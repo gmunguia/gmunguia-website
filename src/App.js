548481bb12f2c8009890651ad0648c52653c8c58
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-03-12 20:47:26 +0100

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1 className="name">Welcome to React</h1>
+          <h2 className="title">zen, millenial<br>web developer</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
