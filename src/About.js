5703980b7acf6706e5075ac9db18f6f6e95059ef
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-04-14 23:01:58 +0200

    Populate "About" page.

--- src/About.js
+++ src/About.js
@@ -1,12 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./About.css";
 
 class About extends Component {
   render() {
     return (
       <div className="About">
-        <Link to="home">{"<-"}</Link>
-        About
+        <div className="About__container">
+          <header className="About__header">
+            <nav className="About__back">
+              <Link to="/">{"↜"}</Link>
+            </nav>
+            <h1>About</h1>
+          </header>
+          <main>
+            <p>
+              I&apos;m a young, talented developer, enjoying the quiet peace in
+              northern Spain.
+            </p>
+            <p>
+              More info in{" "}
+              <a href="https://www.linkedin.com/in/gerardomunguiahoyo/">
+                {" "}
+                my linkedin profile
+              </a>
+            </p>
+          </main>
+        </div>
       </div>
     );
   }
